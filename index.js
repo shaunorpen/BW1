edef593d37d82df1dd5966d08f5dbedc216e7cd5
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-08-27 13:55:18 +0100

    Added FAQ page

--- index.js
+++ index.js
@@ -27,3 +27,21 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+let acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
