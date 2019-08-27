cdd2946a07dbaf6a6e67d0ece99f0d3bbfe238bb
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-08-27 11:33:25 +0100

    Tidy up email id's and javascript functions

--- index.js
+++ index.js
@@ -7,24 +7,18 @@ var modal = document.getElementById("signupModal");
 var btn = document.getElementsByClassName("openSignupModal");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close");
 
-// When the user clicks on the button, open the modal 
-btn[0].onclick = function() {
-  modal.style.display = "block";
-}
-
-btn[1].onclick = function() {
+for (let i = 0; i < btn.length; i++) {
+  btn[i].onclick = function () {
     modal.style.display = "block";
+  }
 }
 
-btn[2].onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+for (let i = 0; i < span.length; i++) {
+  span[i].onclick = function () {
+    modal.style.display = "none";
+  }
 }
 
 // When the user clicks anywhere outside of the modal, close it
