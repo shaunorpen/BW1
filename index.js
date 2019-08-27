3e6f9c87732de86570eda6a003b6a78e205f9925
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-08-27 09:18:44 +0100

    Add open modal from multiple links

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ console.log("Hello from index.js");
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// var btn = document.getElementById("openModal");
+var btn = document.getElementsByClassName("openModal");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
+btn[0].onclick = function() {
   modal.style.display = "block";
+}
+
+btn[1].onclick = function() {
+    modal.style.display = "block";
+}
+
+btn[2].onclick = function() {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
